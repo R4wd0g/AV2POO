82e8c231c068e8fd1ba4349635487841ca83ed19
R4wd0g <wpa.sys@hotmail.com>
2023-12-02 03:10:45 -0300

    .

--- ClienteComunicacao.java
+++ ClienteComunicacao.java
@@ -1,17 +1,52 @@
 import java.io.*;
-import java.net.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+// ... [importações anteriores]
 
 public class ClienteComunicacao {
-    private static final String ENDERECO_SERVIDOR = "localhost";
-    private static final int PORTA_SERVIDOR = 12345;
+    // ... [código anterior]
 
-    public static void main(String[] args) throws IOException {
-        try (Socket socket = new Socket(ENDERECO_SERVIDOR, PORTA_SERVIDOR);
+    public static void main(String[] args) {
+        // ... [código anterior para estabelecer conexão]
+
+        try {
+            Entidade novaEntidade = new Entidade("Teste", "Descrição da Entidade");
+            out.writeObject(novaEntidade);
+
+            Object resposta = in.readObject();
+            System.out.println("Resposta do servidor: " + resposta);
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+public class ClienteComunicacao {
+    private static final String HOST = "localhost"; // Endereço do servidor
+    private static final int PORTA = 12345; // Porta para conectar ao servidor
+
+    public static void main(String[] args) {
+        try (Socket socket = new Socket(HOST, PORTA);
              ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
              ObjectInputStream in = new ObjectInputStream(socket.getInputStream())) {
 
-            // Enviar solicitações e receber respostas do servidor
+            // Exemplo de envio de uma mensagem para o servidor
+            out.writeObject("Olá, servidor!");
 
+            // Exemplo de recebimento de uma resposta do servidor
+            Object resposta = in.readObject();
+            System.out.println("Resposta do servidor: " + resposta);
+
+        } catch (UnknownHostException e) {
+            System.err.println("Host desconhecido: " + HOST);
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.err.println("Não foi possível estabelecer conexão com " + HOST + " na porta " + PORTA);
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            System.err.println("Problema com a classe do objeto recebido");
+            e.printStackTrace();
         }
     }
 }
--- Servidor.java
+++ Servidor.java
@@ -2,21 +2,64 @@ import java.io.*;
 import java.net.*;
 import java.util.concurrent.*;
 
+// ... [importações anteriores]
+
 public class Servidor {
-    private static final int PORTA = 12345;
+    // ... [código anterior]
 
-    public static void main(String[] args) throws IOException {
-        ExecutorService pool = Executors.newFixedThreadPool(10);
-        try (ServerSocket serverSocket = new ServerSocket(PORTA)) {
-            System.out.println("Servidor iniciado na porta " + PORTA);
+    private static class ClientHandler implements Runnable {
+        // ... [código anterior]
 
-            while (true) {
-                Socket clientSocket = serverSocket.accept();
-                pool.execute(new ClientHandler(clientSocket));
+        public void run() {
+            try (ObjectInputStream in = new ObjectInputStream(clientSocket.getInputStream());
+                 ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream())) {
+
+                Object objetoRecebido = in.readObject();
+
+                if (objetoRecebido instanceof Entidade) {
+                    Entidade entidadeRecebida = (Entidade) objetoRecebido;
+                    // Aqui você pode adicionar a lógica para processar a entidade
+                    System.out.println("Entidade recebida: " + entidadeRecebida);
+
+                    // Enviar uma resposta de volta ao cliente
+                    out.writeObject("Entidade recebida com sucesso!");
+                }
+
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    clientSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
+}
 
+
+public class Servidor {
+    private static final int PORTA = 12345; // Porta na qual o servidor escuta
+    private static ExecutorService pool = Executors.newFixedThreadPool(10); // Pool de threads
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(PORTA);
+        System.out.println("Servidor iniciado na porta " + PORTA);
+
+        try {
+            while (true) {
+                Socket clientSocket = serverSocket.accept(); // Aceita conexão do cliente
+                System.out.println("Conexão aceita de " + clientSocket);
+                ClientHandler clientHandler = new ClientHandler(clientSocket);
+                pool.execute(clientHandler); // Executa a thread do cliente
+            }
+        } finally {
+            serverSocket.close();
+        }
+    }
+
+    // Classe interna para lidar com cada cliente em uma thread separada
     private static class ClientHandler implements Runnable {
         private Socket clientSocket;
 
@@ -24,12 +67,12 @@ public class Servidor {
             this.clientSocket = socket;
         }
 
-        @Override
         public void run() {
             try (ObjectInputStream in = new ObjectInputStream(clientSocket.getInputStream());
                  ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream())) {
 
-                // Implemente a lógica de CRUD aqui, processando as solicitações dos clientes
+                // Aqui você pode processar as mensagens recebidas do cliente
+                // Por exemplo, ler objetos, processar solicitações CRUD, etc.
 
             } catch (IOException e) {
                 e.printStackTrace();
